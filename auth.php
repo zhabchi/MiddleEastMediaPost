b719ea8fdccbe77974bd78961083d36ead054c8d
zhabchi <ziad.habchi@gmail.com>
2014-12-16 14:35:25 +0200

    prelaunch
    checkpoint !

--- auth.php
+++ auth.php
@@ -5,7 +5,7 @@ session_start();
 	{
 		header("Location:main.html");
 	}
-	else if($_GET["username"] == "maha" && $_GET["password"] == "hamdan")
+	else if($_GET["username"] == "Mahaintell" && $_GET["password"] == "h1u2n3d4r5e6d7")
 	{	
 		$_SESSION['loggedin'] = 1;
     	$_SESSION['username'] = $_GET["username"];
